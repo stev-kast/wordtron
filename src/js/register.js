189bd377afbee1f0a2cf243ad1cff34f6bbdde31
stevkast <stevkast@gmail.com>
2022-06-02 01:07:26 -0500

    register now working with mongodb

--- src/js/register.js
+++ src/js/register.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const bcrypt = require("bcrypt");
+const axios = require("axios");
 
 const link = path.join(__dirname, "../js/users.json");
 
@@ -16,49 +17,35 @@ document.getElementById("submit-btn").addEventListener("click", function (e) {
   addUser();
 });
 
-const readUsers = async () => {
-  if (fs.existsSync(link)) {
-    let archivo = fs.readFileSync(link, "utf8");
-    let users = JSON.parse(archivo);
-    return await users;
-  }
-};
+const getUserByUsername = async (username) => {
+  let user = await axios.get("http://localhost:3000/user/username/"+username)
+  return user;
+}
 
 async function addUser() {
-  let datos = await readUsers();
-
-  // Crea el archivo de datos si no existe
-  if (datos === undefined) {
-    datos = {
-      users: [],
-      statics: [],
-    };
-
-    let cadena = JSON.stringify(datos);
-    fs.writeFileSync(link, cadena);
-  }
   let newUser = {
     name: document.getElementById("user-name").value,
-    lastName: document.getElementById("user-last-name").value,
+    lastname: document.getElementById("user-last-name").value,
     username: document.getElementById("user-username").value,
-    passwd: document.getElementById("user-password").value,
   };
-  console.log(newUser);
+  users = await getUserByUsername(newUser.username);
   // Verifica si el nombre de usuario ya existe
-  if (!datos.users.some((e) => e.username == newUser.username)) {
-    // Si no existe el nombre de usuario, lo agrega al array de usuarios
-
+  if (users.data.length==0) {
     //Encriptacion del password
-    const hash = await bcrypt.hashSync(newUser.passwd, saltRounds);
-    newUser.passwd = hash;
+    const hash = await bcrypt.hashSync(document.getElementById("user-password").value, saltRounds);
+    newUser.password = hash;
     // Agrega un usuario a la base
-    datos.users.push(newUser);
-    // Pasa de objeto JSON a un string de datos
-    let cadena = JSON.stringify(datos);
-    // Escribe la cadena al archivo datos.JSON
-    fs.writeFileSync(link, cadena);
+    try {
+      // Hace la petición al Rest API
+      let respuesta = await axios.post("http://localhost:3000/user",
+        newUser);
+      // Muestra el resultado
+      console.log(respuesta);
     alert("User Created");
-    return datos;
+    } catch (error) {
+      console.log(error);
+    }
+
   } else {
     alert("Username already in use");
   }
