37039329869ded44bda4c44197e80b5c7790820d
stevkast <stevkast@gmail.com>
2022-06-02 01:19:28 -0500

    login working with mongodb

--- src/js/login.js
+++ src/js/login.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const bcrypt = require("bcrypt");
 const { ipcRenderer } = require("electron");
+const axios = require("axios");
 
 const link = path.join(__dirname, "../js/users.json");
 
@@ -14,42 +15,24 @@ document.getElementById("submit-btn").addEventListener("click", function (e) {
   login();
 });
 
-const readUsers = async () => {
-  if (fs.existsSync(link)) {
-    let archivo = fs.readFileSync(link, "utf8");
-    let users = JSON.parse(archivo);
-    return users;
-  }
-};
-
+const getUserByUsername = async (username) => {
+  let user = await axios.get("http://localhost:3000/user/username/"+username)
+  return user;
+}
 async function login() {
   let account = {
     username: document.getElementById("user-username").value,
     passwd: document.getElementById("user-password").value,
   };
-
-  let datos = await readUsers();
-  if (datos === undefined) {
-    alert("User not found");
-  } else {
-    if (
-      datos.users.some((e) => e.username == account.username) &&
-      datos.users.length > 0
-    ) {
-      // busca si existe el nombre de usuario
-      let ind = await datos.users.findIndex(
-        // busca el index del nombre de usuario en el array de los usuarios
-        (e) => e.username == account.username
-      );
-      let user = datos.users[ind]; // Guarda el usuario con el username que se encontro
-      if (bcrypt.compareSync(account.passwd, user.passwd)) {
-        ipcRenderer.send("openGame", { user: user.username });
-      } else {
-        alert("Invalid credentials");
-        return false;
-      }
+  foundUser = await getUserByUsername(account.username);
+  if (foundUser.data.length > 0) {
+    if (bcrypt.compareSync(account.passwd, foundUser.data[0].password)) {
+      ipcRenderer.send("openGame", { user: foundUser.username });
     } else {
-      alert("User not found");
+      alert("Invalid credentials");
+      return false;
     }
+  } else {
+    alert("User not found");
   }
 }
